4b5b6a41c257db009181577cd5f212448c2992a3
yodd <creatyoo0491@gmail.com>
2024-10-03 00:28:11 +0900

    urls,views and templates HW

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweet
 
-# Create your views here.
+
+def see_all_tweets(request):
+    tweets = Tweet.objects.all()
+    print(tweets)
+    return render(
+        request,
+        "all_tweets.html",
+        {"tweets": tweets, "title": "Hello! this title comes from django!"},
+    )
+
+
+def see_one_tweet(request, tweet_id):
+    return HttpResponse(f"see tweet with id: {tweet_id}")
